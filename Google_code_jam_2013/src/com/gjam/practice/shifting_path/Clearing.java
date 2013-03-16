ca1dd8aa419f49f16988af0736a9e23027cc634e
boyusun <boyusun@163.com>
2013-03-16 13:53:15 -0700

    initialization of the project

---
+++ Google_code_jam_2013/src/com/gjam/practice/shifting_path/Clearing.java
@@ -0,0 +1,11 @@
+package com.gjam.practice.shifting_path;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author boyusun.
+ *         Created 2013-3-16.
+ */
+public class Clearing {
+
+}
---
+++ Google_code_jam_2013/src/com/gjam/practice/shifting_path/ShiftingPath.java
@@ -0,0 +1,21 @@
+package com.gjam.practice.shifting_path;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author boyusun.
+ *         Created 2013-3-16.
+ */
+public class ShiftingPath {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+
+	}
+
+}
